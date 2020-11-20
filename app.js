6a03951ed7d8f07be0532ed3bc4d588982b67a19
duccao <caovanducs@gmail.com>
2020-11-20 16:26:49 +0700

    set up handlebars

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const exhbs = require("express-handlebars");
+
+const app = express();
+
+app.engine(
+  "hbs",
+  exhbs({
+    defaultLayout: "main.hbs",
+    extname: ".hbs",
+    layoutsDir: "views/_layouts",
+    partialsDir: "views/_partials",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/bs4", (req, res) => {
+  console.log(req.query.show);
+  const show = +req.query.show || 0;
+  const visible = show !== 0;
+  res.render("bs4", {
+    layout: false,
+    data: { visible },
+  });
+});
+
+const PORT = 1212;
+app.listen(PORT, (_) => {
+  console.log("App os listening at ", PORT);
+});
